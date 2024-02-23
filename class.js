8e4d6981966be4fcb9ee6f98d2df7e623e2b376c
Zioun <jionkhan0@gmail.com>
2024-02-23 22:18:40 +0600

    added

---
+++ class.js
@@ -0,0 +1,7 @@
+const product = [
+    {id:1, name:'Lenovo laptop', price: 65000},
+    {id:2, name:'MacBook', price: 65000},
+    {id:3, name:'HP', price: 55000},
+    {id:4, name:'Dell', price: 35000}
+]
+
