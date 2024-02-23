eb155fa886c90f4423dfaff64deeddae5aa47643
Zioun <jionkhan0@gmail.com>
2024-02-24 00:10:15 +0600

    added

---
+++ dot.js
@@ -0,0 +1,10 @@
+const person = {
+    name: 'kodom ali',
+    job: 'badam khai',
+    3:'third',
+    'add-dress': 'kochu khat'
+}
+const prop = 'add-dress'
+console.log(person.name);
+console.log(person['3']);
+console.log(person[prop]);
